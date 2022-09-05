c875c605a96f4b69a474b38deb6fbdedde595bcc
Ronni Kahalani <ronni.kahalani@gmail.com>
2022-09-05 05:21:51 +0200

    Updated the rest endpoint features

--- js/rest-server.js
+++ js/rest-server.js
@@ -5,29 +5,46 @@
  */
 // File O/I feature.
 const path = require('path');
+const fs = require('fs');
 // Web server feature.
 const express = require('express');
 // Web server app instance.
 const app = express();
 
 // Used HTTP port numbers
-const HTTP_PORT = parseInt(process.env.HTTP_PORT) || 3000;
+const HTTP_PORT = parseInt(process.env.HTTP_PORT) || 8080;
 
 // HTTP server example setup.
 app.get('/api/v1/planets', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.sendFile(path.resolve(__dirname, '../json/planets.json'))
+    fs.readFile('../json/planets.json', 'utf-8', (err, data) => {
+        let json = JSON.parse(data);
+        console.log("Sending planets");
+        res.end(JSON.stringify(json));
+    })
 });
 app.get('/api/v1/spaceships', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.sendFile(path.resolve(__dirname, '../json/spaceships.json'))
+    fs.readFile('../json/spaceships.json', 'utf-8', (err, data) => {
+        let json = JSON.parse(data);
+        console.log("Sending spaceships");
+        res.end(JSON.stringify(json));            
+    })
 });
 app.get('/api/v1/customers', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.sendFile(path.resolve(__dirname, '../json/customers.json'))
+    fs.readFile('../json/customers.json', 'utf-8', (err, data) => {
+        let json = JSON.parse(data);
+        console.log("Sending customers");
+        res.end(JSON.stringify(json));            
+    })
 });
 app.get('/api/v1/reservations', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.sendFile(path.resolve(__dirname, '../json/reservations.json'))
+    fs.readFile('../json/reservations.json', 'utf-8', (err, data) => {
+        let json = JSON.parse(data);
+        console.log("Sending reservations");
+        res.end(JSON.stringify(json));            
+    })
 });
 app.listen(HTTP_PORT, () => console.log(`HTTP server listening at http://localhost:${HTTP_PORT}`));
