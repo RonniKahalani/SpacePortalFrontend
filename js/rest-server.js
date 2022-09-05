89352d7075c764caafe79bc18e427b4ee4348f78
Ronni Kahalani <ronni.kahalani@gmail.com>
2022-09-05 07:00:51 +0200

    Added cors support

--- js/rest-server.js
+++ js/rest-server.js
@@ -7,43 +7,48 @@
 const fs = require('fs');
 // Web server feature.
 const express = require('express');
+// CORS feature.
+const cors = require('cors');
 // Web server app instance.
 const app = express();
 
 // Used HTTP port numbers
 const HTTP_PORT = parseInt(process.env.HTTP_PORT) || 8080;
 
-// HTTP server example setup.
-app.get('/api/v1/planets', (req, res) => {
+// Returns planets.
+app.get('/api/v1/planets', cors(), (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     fs.readFile('../json/planets.json', 'utf-8', (err, data) => {
         let json = JSON.parse(data);
-        console.log("Sending planets");
-        res.end(JSON.stringify(json));
+        console.log("Sending planets: " + JSON.stringify(json));
+        res.json(json);
     })
 });
-app.get('/api/v1/spaceships', (req, res) => {
+// Returns spaceships.
+app.get('/api/v1/spaceships', cors(), (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     fs.readFile('../json/spaceships.json', 'utf-8', (err, data) => {
         let json = JSON.parse(data);
-        console.log("Sending spaceships");
-        res.end(JSON.stringify(json));            
+        console.log("Sending spaceships: " + JSON.stringify(json));
+        res.json(json);
     })
 });
-app.get('/api/v1/customers', (req, res) => {
+// Returns customers.
+app.get('/api/v1/customers', cors(), (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     fs.readFile('../json/customers.json', 'utf-8', (err, data) => {
         let json = JSON.parse(data);
-        console.log("Sending customers");
-        res.end(JSON.stringify(json));            
+        console.log("Sending customers: " + JSON.stringify(json));
+        res.json(json);
     })
 });
-app.get('/api/v1/reservations', (req, res) => {
+// Returns reservations.
+app.get('/api/v1/reservations', cors(), (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     fs.readFile('../json/reservations.json', 'utf-8', (err, data) => {
         let json = JSON.parse(data);
-        console.log("Sending reservations");
-        res.end(JSON.stringify(json));            
+        console.log("Sending reservations: " + JSON.stringify(json));
+        res.json(json);
     })
 });
 app.listen(HTTP_PORT, () => console.log(`HTTP server listening at http://localhost:${HTTP_PORT}`));
