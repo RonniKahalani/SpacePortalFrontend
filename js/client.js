f38a1fb2db11fa19230405da7f15f4844dc83b0d
Ronni Kahalani <ronni.kahalani@gmail.com>
2022-09-22 22:11:15 +0200

    Cleanup

--- js/client.js
+++ js/client.js
@@ -14,48 +14,64 @@ class SocketClient {
         this.WS_URL = this.endpointUrl;
         this.ws = new WebSocket(this.WS_URL);
         
-        // Called when a connection, to the server, is open.
         this.ws.onopen = () => console.log(`Connected to ${this.WS_URL}`);
+        this.ws.onmessage = (ev) => this.onMessageEvent(ev);
+        this.ws.onerror = (ev) =>this.onErrorEvent(ev);
+        this.ws.onclose = (ev) => this.onCloseEvent(ev);
+    }
 
-        // Called when a message arrives from the server.
-        this.ws.onmessage = (ev) => {
-            let packet = JSON.parse(ev.data);
-
-            console.log(`Got packet from server:${JSON.stringify(packet)}`);
-            console.log(`The message is: ${packet.msg}`);
-
-            let link = (packet.link) ? packet.link : (packet.image) ? packet.image : null;
-            let imageDiv = (packet.image) ? `<div class="col-3">
-           <img class="socket-image float-end" src="${packet.image}">
-           </div>` : '';
-
-            let linkStart = (link) ? `<a href="${link}" class="socket-link" target="_blank">` : '';
-            let linkEnd = (link) ? `</a>` : '';
-
-            let msg = `${linkStart}
-            <div class="card mt-1 mb-1">
-                    <div class="container row btn-dark m-0 p-0">
-                        <div class="col">
-                            <div class="row">
-                                <div class="socket-user col">${packet.date} ${packet.user}</div>
-                            </div>
-                            <div class="socket-msg">${packet.msg}</div>
-                        </div>
-                        ${(imageDiv) ? imageDiv : ''}
-                    </div>
-                </div>
-            ${linkEnd}`
-
-            $('#messages').prepend(msg);
-            $('#message-count').text(++this.messageCounter);
-            $("#message-heading").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
-        }
-
-        this.ws.onerror = (ev) => $('#messages').prepend(`<div>${ev}</div>`);
-        this.ws.onclose = (ev) => {
-            $('#messages').prepend(`<div>${ev}</div>`)
+    /**
+     * Handles close event.
+     * @param {*} ev 
+     */
+         onCloseEvent(ev) {
+            $('#messages').prepend(`<div>${ev}</div>`);
             this.ws.close();
-        }
+         }
+    
+    /**
+     * Handles error events.
+     * @param {*} ev 
+     */
+     onErrorEvent(ev) {
+        $('#messages').prepend(`<div>${ev}</div>`);
+     }
+
+    /**
+     * Handles incoming message events.
+     * @param {*} ev 
+     */
+    onMessageEvent(ev) {
+        let packet = JSON.parse(ev.data);
+
+        console.log(`Got packet from server:${JSON.stringify(packet)}`);
+        console.log(`The message is: ${packet.msg}`);
+
+        let link = (packet.link) ? packet.link : (packet.image) ? packet.image : null;
+        let imageDiv = (packet.image) ? `<div class="col-3">
+       <img class="socket-image float-end" src="${packet.image}">
+       </div>` : '';
+
+        let linkStart = (link) ? `<a href="${link}" class="socket-link" target="_blank">` : '';
+        let linkEnd = (link) ? `</a>` : '';
+
+        let msg = `${linkStart}
+        <div class="card mt-1 mb-1">
+                <div class="container row btn-dark m-0 p-0">
+                    <div class="col">
+                        <div class="row">
+                            <div class="socket-user col">${packet.date} ${packet.user}</div>
+                        </div>
+                        <div class="socket-msg">${packet.msg}</div>
+                    </div>
+                    ${(imageDiv) ? imageDiv : ''}
+                </div>
+            </div>
+        ${linkEnd}`
+
+        $('#messages').prepend(msg);
+        $('#message-count').text(++this.messageCounter);
+        $("#message-heading").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
     }
 
     /**
